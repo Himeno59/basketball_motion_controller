6d2989bf5896857ba1e06b4e54ce480b874f9bbb
himeno <himeno@jsk.imi.i.u-tokyo.ac.jp>
2024-06-05 14:14:00 +0900

    add eePosePort

--- basketball_motion_controller/rtc/BasketballMotionController/BasketballMotionController.cpp
+++ basketball_motion_controller/rtc/BasketballMotionController/BasketballMotionController.cpp
@@ -2,26 +2,33 @@
 
 BasketballMotionController::BasketballMotionController(RTC::Manager* manager):
   RTC::DataFlowComponentBase(manager),
-  m_qRefIn_("qRefIn", m_qRef_),
-  m_tauRefIn_("tauRefIn", m_tauRef_),
-  m_qActIn_("qActIn", m_qAct_),
-  m_dqActIn_("dqActIn", m_dqAct_),
-  m_tauActIn_("tauActIn", m_tauAct_),
-  m_qOut_("q", m_q_),
-  m_tauOut_("tauOut", m_tau_),
+  m_objRefIn_("objRefIn", m_objRef_),
+  m_rarm_eePoseOut_("rarm_eePoseOut", m_rarm_eePose_),
+  // m_eePoseIn_("EEPoseOut", m_eePose_),
+  
+  // m_qRefIn_("qRefIn", m_qRef_),
+  // m_tauRefIn_("tauRefIn", m_tauRef_),
+  // m_qActIn_("qActIn", m_qAct_),
+  // m_dqActIn_("dqActIn", m_dqAct_),
+  // m_tauActIn_("tauActIn", m_tauAct_),
+  // m_qOut_("q", m_q_),
+  // m_tauOut_("tauOut", m_tau_),
   m_basketballmotionControllerServicePort_("BasketballMotionControllerService")
 {
   this->m_service0_.setComp(this);
 }
 
 RTC::ReturnCode_t BasketballMotionController::onInitialize(){
-  addInPort("qRefIn", this->m_qRefIn_);
-  addInPort("tauRefIn", this->m_tauRefIn_);
-  addInPort("qActIn", this->m_qActIn_);
-  addInPort("dqActIn", this->m_dqActIn_);
-  addInPort("tauActIn", this->m_tauActIn_);
-  addOutPort("q", this->m_qOut_);
-  addOutPort("tauOut", this->m_tauOut_);
+  addInPort("objRefIn", this->m_objRefIn_);
+  addOutPort("rarm_eePoseOut", this->m_rarm_eePoseOut_);
+  
+  // addInPort("qRefIn", this->m_qRefIn_);
+  // addInPort("tauRefIn", this->m_tauRefIn_);
+  // addInPort("qActIn", this->m_qActIn_);
+  // addInPort("dqActIn", this->m_dqActIn_);
+  // addInPort("tauActIn", this->m_tauActIn_);
+  // addOutPort("q", this->m_qOut_);
+  // addOutPort("tauOut", this->m_tauOut_);
   this->m_basketballmotionControllerServicePort_.registerProvider("service0", "BasketballMotionControllerService", this->m_service0_);
   addPort(this->m_basketballmotionControllerServicePort_);
 
@@ -29,47 +36,115 @@ RTC::ReturnCode_t BasketballMotionController::onInitialize(){
 }
 
 RTC::ReturnCode_t BasketballMotionController::onExecute(RTC::UniqueId ec_id){
-  std::cerr << "BasketballMotionController rtc onExecute" << std::endl;
+  // std::cerr << "BasketballMotionController rtc onExecute" << std::endl;
 
-  if (this->m_qRefIn_.isNew()){
-    this->m_qRefIn_.read();
+  // if (this->m_qRefIn_.isNew()){
+  //   this->m_qRefIn_.read();
+  // }
+
+  // if (this->m_tauRefIn_.isNew()){
+  //   this->m_tauRefIn_.read();
+  // }
+
+  // if (this->m_qActIn_.isNew()){
+  //   this->m_qActIn_.read();
+  // }
+
+  // if (this->m_dqActIn_.isNew()){
+  //   this->m_dqActIn_.read();
+  // }
+
+  // if (this->m_tauActIn_.isNew()){
+  //   this->m_tauActIn_.read();
+  // }
+
+  // {
+  //   m_q_.tm = m_qRef_.tm;
+  //   m_q_.data.length(m_qRef_.data.length());
+  //   for (int i = 0 ; i < m_q_.data.length(); i++){
+  //     m_q_.data[i] = m_qRef_.data[i];
+  //   }
+  //   this->m_qOut_.write();
+  // }
+
+  // {
+  //   m_tau_.tm = m_qRef_.tm;
+  //   m_tau_.data.length(m_tauRef_.data.length());
+  //   for (int i = 0 ; i < m_q_.data.length(); i++){
+  //     m_tau_.data[i] = m_tauRef_.data[i];
+  //   }
+  //   this->m_tauOut_.write();
+  // }
+
+  // readInPortData
+  if (this->m_objRefIn_.isNew()){
+    this->m_objRefIn_.read();
   }
 
-  if (this->m_tauRefIn_.isNew()){
-    this->m_tauRefIn_.read();
-  }
+  // generate targetEEPose
+  this->genTargetEEPose();
 
-  if (this->m_qActIn_.isNew()){
-    this->m_qActIn_.read();
-  }
-
-  if (this->m_dqActIn_.isNew()){
-    this->m_dqActIn_.read();
-  }
-
-  if (this->m_tauActIn_.isNew()){
-    this->m_tauActIn_.read();
-  }
-
+  // writeOutPortData
   {
-    m_q_.tm = m_qRef_.tm;
-    m_q_.data.length(m_qRef_.data.length());
-    for (int i = 0 ; i < m_q_.data.length(); i++){
-      m_q_.data[i] = m_qRef_.data[i];
-    }
-    this->m_qOut_.write();
-  }
-
-  {
-    m_tau_.tm = m_qRef_.tm;
-    m_tau_.data.length(m_tauRef_.data.length());
-    for (int i = 0 ; i < m_q_.data.length(); i++){
-      m_tau_.data[i] = m_tauRef_.data[i];
-    }
-    this->m_tauOut_.write();
+    this->m_rarm_eePoseOut_.write();
   }
 
   return RTC::RTC_OK;
+}
+
+// void BasketballMotionController::genTargetEEPose() const {
+//   RTC::TimedPose3DSeq targetEEPose; // 0:rleg, 1:lleg, 2:rarm, 3:larm
+//   // rarm
+//   // point
+//   targetEEPose[2].position.x = 0.0;
+//   targetEEPose[2].position.y = 0.0;
+//   targetEEPose[2].position.z = 0.0;
+//   // orientation
+//   targetEEPose[2].orientation.r = 0.0;
+//   targetEEPose[2].orientation.p = 0.0;
+//   targetEEPose[2].orientation.y = 0.0;
+  
+//   // RTC::TimedPose3D rarm_targetEEPose;
+//   // RTC::TimedPose3D larm_targetEEPose;
+//   // RTC::TimedPose3D rleg_targetEEPose;
+//   // RTC::TimedPose3D lleg_targetEEPose;  
+//   // // point
+//   // ratm_targetEEPose.position.x = 0.0;
+//   // ratm_targetEEPose.position.y = 0.0;
+//   // ratm_targetEEPose.position.z = 0.0;
+  
+//   // // orientation
+//   // ratm_targetEEPose.orientation.r = 0.0;
+//   // ratm_targetEEPose.orientation.p = 0.0;
+//   // ratm_targetEEPose.orientation.y = 0.0;
+      
+//   for(int i=0;i<4;i++){
+//     // 両手を与えるようにするとして、脚はどうする?
+//     m_eePose_[i] = targetEEPose[i];
+//   }
+// }
+
+void BasketballMotionController::genTargetEEPose() {
+  RTC::TimedPose3D targetEEPose;
+  // point
+  targetEEPose.data.position.x = 0.0;
+  targetEEPose.data.position.y = 0.0;
+  targetEEPose.data.position.z = 0.0;
+  // orientation
+  targetEEPose.data.orientation.r = 0.0;
+  targetEEPose.data.orientation.p = 0.0;
+  targetEEPose.data.orientation.y = 0.0;
+
+  m_rarm_eePose_ = targetEEPose;
+
+  std::cout << "[targetEEPose]" << std::endl;
+  std::cout << "position.x: " << m_rarm_eePose_.data.position.x << std::endl;
+  std::cout << "position.y: " << m_rarm_eePose_.data.position.y << std::endl;
+  std::cout << "porision.z: " << m_rarm_eePose_.data.position.z << std::endl;
+  std::cout << "orientation.r: " << m_rarm_eePose_.data.orientation.r << std::endl;
+  std::cout << "orientation.p: " << m_rarm_eePose_.data.orientation.p << std::endl;
+  std::cout << "orientation.y: " << m_rarm_eePose_.data.orientation.y << std::endl;
+  
 }
 
 bool BasketballMotionController::basketballmotionParam(const double data){
--- basketball_motion_controller/rtc/BasketballMotionController/BasketballMotionController.h
+++ basketball_motion_controller/rtc/BasketballMotionController/BasketballMotionController.h
@@ -7,6 +7,9 @@
 #include <rtm/DataInPort.h>
 #include <rtm/CorbaPort.h>
 #include <rtm/idl/BasicDataType.hh>
+#include <rtm/idl/ExtendedDataTypes.hh>
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
 
 #include <basketball_motion_controller_msgs/idl/BasketballMotionController.hh>
 
@@ -15,24 +18,43 @@
 class BasketballMotionController : public RTC::DataFlowComponentBase{
 protected:
 
-  RTC::TimedDoubleSeq m_qRef_;
-  RTC::InPort<RTC::TimedDoubleSeq> m_qRefIn_;
-  RTC::TimedDoubleSeq m_tauRef_;
-  RTC::InPort<RTC::TimedDoubleSeq> m_tauRefIn_;
-  RTC::TimedDoubleSeq m_qAct_;
-  RTC::InPort<RTC::TimedDoubleSeq> m_qActIn_;
-  RTC::TimedDoubleSeq m_dqAct_;
-  RTC::InPort<RTC::TimedDoubleSeq> m_dqActIn_;
-  RTC::TimedDoubleSeq m_tauAct_;
-  RTC::InPort<RTC::TimedDoubleSeq> m_tauActIn_;
-  RTC::TimedDoubleSeq m_q_;
-  RTC::OutPort<RTC::TimedDoubleSeq> m_qOut_;
-  RTC::TimedDoubleSeq m_tau_;
-  RTC::OutPort<RTC::TimedDoubleSeq> m_tauOut_;
+  // inport
+  // ボールの予測軌道、速度
+  RTC::TimedDoubleSeq m_objRef_;
+  RTC::InPort<RTC::TimedDoubleSeq> m_objRefIn_;
+  
+  /* RTC::TimedDoubleSeq m_qRef_; */
+  /* RTC::InPort<RTC::TimedDoubleSeq> m_qRefIn_; */
+  /* RTC::TimedDoubleSeq m_tauRef_; */
+  /* RTC::InPort<RTC::TimedDoubleSeq> m_tauRefIn_; */
+  /* RTC::TimedDoubleSeq m_qAct_; */
+  /* RTC::InPort<RTC::TimedDoubleSeq> m_qActIn_; */
+  /* RTC::TimedDoubleSeq m_dqAct_; */
+  /* RTC::InPort<RTC::TimedDoubleSeq> m_dqActIn_; */
+  /* RTC::TimedDoubleSeq m_tauAct_; */
+  /* RTC::InPort<RTC::TimedDoubleSeq> m_tauActIn_; */
+
+  // outport
+  // refEEPose
+  // 時系列のデータにする必要はある？
+  // todo: これだと一つだけしか送れないので、TimedPose3DSeqという型を作成する
+  RTC::TimedPose3D m_rarm_eePose_;
+  RTC::OutPort<RTC::TimedPose3D> m_rarm_eePoseOut_;
+  /* RTC::TimedPose3DSeq m_eePose_; */
+  /* RTC::OutPort<RTC::TimedPose3DSeq> m_eePoseOut_; */
+  
+  /* RTC::TimedDoubleSeq m_q_; */
+  /* RTC::OutPort<RTC::TimedDoubleSeq> m_qOut_; */
+  /* RTC::TimedDoubleSeq m_tau_; */
+  /* RTC::OutPort<RTC::TimedDoubleSeq> m_tauOut_; */
+  /* RTC::TimedDoubleSeq m_ */
 
   BasketballMotionControllerService_impl m_service0_;
   RTC::CorbaPort m_basketballmotionControllerServicePort_;
 
+protected:
+  void genTargetEEPose();
+  
 public:
   BasketballMotionController(RTC::Manager* manager);
   virtual RTC::ReturnCode_t onInitialize();
