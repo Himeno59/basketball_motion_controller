ee8eeea098b4a781359943d497504c19fa6f7491
himeno <himeno@jsk.imi.i.u-tokyo.ac.jp>
2024-06-19 13:08:43 +0900

    fix

--- basketball_motion_controller/scripts/test_vision.sh
+++ basketball_motion_controller/scripts/test_vision.sh
@@ -9,23 +9,19 @@ new-window nameserver "rm -f /tmp/omninames-* && omniNames -start 15005 -datadir
 sleep 0.5 # wait for roscore
 new-window choreonoid "rossetlocal && rossetip && ${SOURCE_SCRIPT} && roslaunch auto_stabilizer_config choreonoid_JAXON_RED_WITH_MSLHAND.launch"
 sleep 1.0 # wait for clock and spawning models
-new-window hrpsys "rossetlocal && rossetip && ${SOURCE_SCRIPT} && roslaunch auto_stabilizer_config hrpsys_JAXON_RED_WITH_MSLHAND.launch"
+new-window hrpsys "rossetlocal && rossetip && ${SOURCE_SCRIPT} && roslaunch auto_stabilizer_config hrpsys_JAXON_RED_WITH_MSLHAND_for_basketball.launch"
+
+new-window workspace1
+new-window workspace2
 
 new-window rviz "${SOURCE_SCRIPT} && rviz -d $(rospack find auto_stabilizer_config)/rviz_config/JAXON_RED_WITH_MSLHAND.rviz"
 new-window rosbag "${SOURCE_SCRIPT} && roscd basketball_motion/rosbag "
 sleep 1.0
 new-window hsi_color_filter "${SOURCE_SCRIPT} && roslaunch jsk_pcl_ros hsi_color_filter.launch basketball_color:=orange ball_detect:=true"
 new-window calc-pointcloud-centroid "${SOURCE_SCRIPT} && roslaunch jsk_pcl_ros_utils sample_centroid_publisher.launch INPUT:=/orange/hsi_output"
-# new-window fix-centroid "${SOURCE_SCRIPT} && rosrun basketball_motion calc-centroid.py"
-new-window object_trajectory_estimator "${SOURCE_SCRIPT} && roslaunch basketball_motion object_trajectory_estimator.launch"
+new-window object_trajectory_estimator "${SOURCE_SCRIPT} && roslaunch object_trajectory_estimator object_trajectory_estimator.launch"
 new-window ros-topic "${SOURCE_SCRIPT} && rostopic list"
+new-window plot "${SOURCE_SCRIPT} && rosrun rqt_plot rqt_plot"
 
 
-# new-window rviz "${SOURCE_SCRIPT} && rossetjaxon_red && rviz -d $(rospack find auto_stabilizer_config)/rviz_config/JAXON_RED_WITH_MSLHAND.rviz"
-# new-window rosbag "${SOURCE_SCRIPT} && rossetjaxon_red && roscd basketball_motion/rosbag "
-# sleep 1.0
-# new-window hsi_color_filter "${SOURCE_SCRIPT} && rossetjaxon_red && roslaunch jsk_pcl_ros hsi_color_filter.launch basketball_color:=orange ball_detect:=true"
-# new-window calc-pointcloud-centroid "${SOURCE_SCRIPT} && rossetjaxon_red && roslaunch jsk_pcl_ros_utils sample_centroid_publisher.launch INPUT:=/orange/hsi_output"
-# # new-window fix-centroid "${SOURCE_SCRIPT} && rosrun basketball_motion calc-centroid.py"
-# new-window object_trajectory_estimator "${SOURCE_SCRIPT} && rossetjaxon_red && roslaunch basketball_motion object_trajectory_estimator.launch"
-# new-window ros-topic "${SOURCE_SCRIPT} && rossetjaxon_red && rostopic list"
+
